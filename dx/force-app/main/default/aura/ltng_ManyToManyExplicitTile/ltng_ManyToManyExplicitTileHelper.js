eaf3d4a4abd2773fd01525dd36348900999a6cbd
Paul Roth <paulroth3d@gmail.com>
2018-10-11 05:00:29 -0500

    UPDATE: rough working showing in accordions.

--- dx/force-app/main/default/aura/ltng_ManyToManyExplicitTile/ltng_ManyToManyExplicitTileHelper.js
+++ dx/force-app/main/default/aura/ltng_ManyToManyExplicitTile/ltng_ManyToManyExplicitTileHelper.js
@@ -1,6 +1,16 @@
 ({
-		doSomething : function(component, helper){
+		initializeComponent : function(component, helper){
 			helper.noop();
+
+			$A.log('initializeComponent');
+
+			var childRelationship = component.get('v.childrenRelationship');
+			if ( childRelationship) {
+				var fields = childRelationship.junctionObjectFields;
+				$A.log(fields);
+				var junctionFields = component.get('v.childrenRelationship.junctionObjectSpecificFields').split(';');
+				component.set('v.junctionFields', junctionFields);
+			}
 		},
 
 		//-- convenience functions
