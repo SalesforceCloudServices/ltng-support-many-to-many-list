c2f9c5091362d90e848478dfb1f2d3585c3d0fc7
Paul Roth <paulroth3d@gmail.com>
2018-10-11 05:14:29 -0500

    UPDATE: working explicit fields

--- dx/force-app/main/default/aura/ltng_ManyToManyExplicitTile/ltng_ManyToManyExplicitTileController.js
+++ dx/force-app/main/default/aura/ltng_ManyToManyExplicitTile/ltng_ManyToManyExplicitTileController.js
@@ -2,5 +2,13 @@
 		init : function(component, event, helper){
 				$A.log('Component initialized');
 				helper.initializeComponent(component, helper);
+		},
+
+		handleObjectClick : function(component, event, helper){
+			helper.navigateToRecord(component, helper);
+		},
+	
+		handleJunctionClick : function(component, event, helper){
+			helper.navigateToJunction(component, helper);
 		}
 })
--- dx/force-app/main/default/aura/ltng_ManyToManyExplicitTile/ltng_ManyToManyExplicitTileHelper.js
+++ dx/force-app/main/default/aura/ltng_ManyToManyExplicitTile/ltng_ManyToManyExplicitTileHelper.js
@@ -12,6 +12,34 @@
 				component.set('v.junctionFields', junctionFields);
 			}
 		},
+		
+		/**
+		 * Navigates to the record.
+		 */
+		navigateToRecord : function(component, helper){
+			helper.noop();
+			
+			var recordId = component.get('v.recordId');
+			var event = $A.get('e.force:navigateToSObject');
+			event.setParams({
+				recordId: recordId
+			});
+			event.fire();
+		},
+		
+		/**
+		 * Navigates the user to the junction object
+		 **/
+		navigateToJunction : function(component, helper) {
+			helper.noop();
+	
+			var recordId = component.get('v.junctionId');
+			var event = $A.get('e.force:navigateToSObject');
+			event.setParams({
+				recordId: recordId
+			});
+			event.fire();
+		},
 
 		//-- convenience functions
 		noop : function(){}
