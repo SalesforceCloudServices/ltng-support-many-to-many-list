9a1bd612adad7751a4169515cad57b4f23f9e181
Paul Roth <paulroth3d@gmail.com>
2018-10-25 17:27:46 -0500

    INTERNAL: update sample data and queries.

--- data/exportData.sh
+++ data/exportData.sh
@@ -3,6 +3,8 @@
 
 sfdx force:data:tree:export -d tree -u manyToManyDev -q queries/ltng_M2M_Account__c.txt
 sfdx force:data:tree:export -d tree -u manyToManyDev -q queries/ltng_M2M_Contact__c.txt
-sfdx force:data:tree:export -d tree -u manyToManyDev -q queries/ltng_M2M_Partner__c.txt
 sfdx force:data:tree:export -d tree -u manyToManyDev -q queries/ltng_M2M_Relationship__c.txt
+
+sfdx force:data:soql:query -q "$(< queries/ltng_M2M_AccountContactRelationship__c.txt)" --json | JSON > tree/ltng_M2M_AccountContactRelationship__c.json
+sfdx force:data:soql:query -q "$(< queries/ltng_M2M_Partner__c.txt )" --json | JSON > tree/ltng_M2M_Partner__c.json
 
